c5ae5a7d84bd8747296e93e33ca92fc677e09eab
Danilo <emaildedanilo@gmail.com>
2019-07-12 11:33:10 -0400

    Render all pizzas in the DOM

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     }
 
     // write query for all pizza
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     // make query and get result
     $result = mysqli_query($conn, $sql);
@@ -17,7 +17,7 @@
     // fetch the resulting rows as an array
     $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    // free the resut from memory
+    // free the result from memory
     mysqli_free_result($result);
 
     // close the connection
@@ -29,6 +29,28 @@
 <html lang="en">
 
     <?php include('templates/header.php'); ?>
+
+    <h4 class="center grey-text">Pizzas!</h4>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza) { ?>
+
+                <div class="col s6 md3">
+                    <div class="card z-deth-0">
+                        <div class="card-content center">
+                            <h6> <?php echo htmlspecialchars($pizza['title']); ?> </h6>
+                            <div> <?php echo htmlspecialchars($pizza['ingredients']); ?> </div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+
+            <?php } ?>
+        </div>
+    </div>
 
     <?php include('templates/footer.php'); ?>
     
